78618192e7c79d89da63d45fad8052280d134c75
HrOokami <dev@hrookami.icu>
2024-01-28 06:02:58 +0300

    We got camera! Gravity! Controls!

---
+++ addons/planet_plugin/scenes/planet/area_gravity.gd
@@ -0,0 +1,40 @@
+extends Area3D
+
+class_name AreaGravity
+
+@onready var gravity_shape: CollisionShape3D = $GravityShape
+@onready var debug_target = $DebugTarget
+@onready var debug_forward = $DebugForward
+
+var vec_target = Vector3()
+var vec_forward = Vector3()
+
+func _ready():
+	pass
+
+func _process(delta):
+	var mesh = debug_target.mesh as ImmediateMesh
+	var mesh2 = debug_forward.mesh as ImmediateMesh
+	
+	mesh.clear_surfaces()
+	mesh.surface_begin(Mesh.PRIMITIVE_LINES)
+	mesh.surface_add_vertex(Vector3(0, 0, 0))
+	mesh.surface_add_vertex(vec_target)
+	mesh.surface_end()
+	
+	mesh2.clear_surfaces()
+	mesh2.surface_begin(Mesh.PRIMITIVE_LINES)
+	mesh2.surface_add_vertex(Vector3(0, 0, 0))
+	mesh2.surface_add_vertex(vec_forward)
+	mesh2.surface_end()
+	
+
+func get_front_gvec_from_gpoint(gtarget: Vector3):
+	var local_pos = to_local(gtarget)
+	vec_target = local_pos
+	var radius = (gravity_shape.shape as SphereShape3D).radius
+	
+	var vec = Vector2(local_pos.x, local_pos.z).orthogonal().normalized()
+	var local_forward = Vector3(vec.x, 0, vec.y) + local_pos
+	vec_forward = local_forward
+	return to_global(local_forward)
--- scenes/game/character/character.gd
+++ scenes/game/character/character.gd
@@ -29,24 +29,60 @@ var rot_y = 0
 
 @onready var debug_velocity: MeshInstance3D = $DebugVelocity
 @onready var debug_gravity: MeshInstance3D = $DebugGravity
+@onready var debug_forward = $DebugForward
+@onready var debug_up = $DebugUp
+@onready var debug_right = $DebugRight
+
+var current_planet_area: AreaGravity
+var vec_forward = Vector3()
+var vec_right = Vector3()
+var vel_gravity = Vector3()
 
 func _process(delta):
 	var mesh = debug_velocity.mesh as ImmediateMesh
 	var mesh2 = debug_gravity.mesh as ImmediateMesh
+	var mesh3 = debug_forward.mesh as ImmediateMesh
+	var mesh4 = debug_up.mesh as ImmediateMesh
+	var mesh5 = debug_right.mesh as ImmediateMesh
+	
+	var current_basis = transform.basis
+	transform.basis = Basis.IDENTITY
+	
+	var gzero = to_global(Vector3(0, 0, 0))
 	
 	mesh.clear_surfaces()
 	mesh.surface_begin(Mesh.PRIMITIVE_LINES)
-	mesh.surface_add_vertex(Vector3(0, 0, 0))
-	mesh.surface_add_vertex(velocity)
+	mesh.surface_add_vertex(gzero)
+	mesh.surface_add_vertex(to_global(velocity))
 	mesh.surface_end()
 	
 	var gravity = PhysicsServer3D.body_get_direct_state(get_rid()).total_gravity
 	
 	mesh2.clear_surfaces()
 	mesh2.surface_begin(Mesh.PRIMITIVE_LINES)
-	mesh2.surface_add_vertex(Vector3(0, 0, 0))
-	mesh2.surface_add_vertex(gravity)
+	mesh2.surface_add_vertex(gzero)
+	mesh2.surface_add_vertex(to_global(gravity))
 	mesh2.surface_end()
+	
+	mesh3.clear_surfaces()
+	mesh3.surface_begin(Mesh.PRIMITIVE_LINES)
+	mesh3.surface_add_vertex(gzero)
+	mesh3.surface_add_vertex(to_global(vec_forward))
+	mesh3.surface_end()
+	
+	mesh4.clear_surfaces()
+	mesh4.surface_begin(Mesh.PRIMITIVE_LINES)
+	mesh4.surface_add_vertex(gzero)
+	mesh4.surface_add_vertex(to_global(up_direction))
+	mesh4.surface_end()
+	
+	mesh5.clear_surfaces()
+	mesh5.surface_begin(Mesh.PRIMITIVE_LINES)
+	mesh5.surface_add_vertex(gzero)
+	mesh5.surface_add_vertex(to_global(vec_right))
+	mesh5.surface_end()
+	
+	transform.basis = current_basis
 
 func _physics_process(delta):
 	_handle_physics(delta)
@@ -57,15 +93,28 @@ func _handle_physics(delta: float):
 	
 	if gravity.length() > 0.01:
 		up_direction = -gravity.normalized()
+		
+	if current_planet_area:
+		var zero_global = to_global(Vector3())
+		var g_forward = current_planet_area.get_front_gvec_from_gpoint(zero_global)
+		vec_forward = to_local(g_forward).normalized()
+		var plane = Plane(up_direction)
+		vec_forward = plane.project(vec_forward).normalized()
+		vec_right = vec_forward.cross(up_direction)
+		transform.basis = Basis(vec_forward, up_direction, vec_right)
+		
+	velocity = Vector3()
 	
 	var jump_pressed = Input.is_action_just_pressed("jump")
 	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += gravity * delta
+		vel_gravity += gravity * delta
 		
 		if animation_player.current_animation != JUMP_LOOP_ANIMATION:
 			animation_player.current_animation = JUMP_LOOP_ANIMATION
+	else:
+		vel_gravity = Vector3()
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -78,15 +127,17 @@ func _handle_physics(delta: float):
 	else:
 		animation_player.current_animation = IDLE_ANIMATION
 	
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (transform.basis * Vector3(-input_dir.y, 0, input_dir.x)).normalized()
 	if direction:
 		pass
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
+		velocity += direction * SPEED
 	else:
 		pass
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	velocity += vel_gravity
 
 	move_and_slide()
 
@@ -107,6 +158,15 @@ func _input(event):
 func _handle_mouse_motion(event: InputEventMouseMotion):
 	rot_x += event.relative.x * LOOKAROUND_SPEED
 	rot_y += -event.relative.y * LOOKAROUND_SPEED
-	camera_3d.transform.basis = Basis() # reset rotation
-	camera_3d.rotate_object_local(Vector3(0, 1, 0), rot_x) # first rotate in Y
-	camera_3d.rotate_object_local(Vector3(-1, 0, 0), rot_y) # then rotate in X
+	var vecyplane = Vector3(cos(rot_x)* camera_distance, 1.5, sin(rot_x) * camera_distance)
+	var up = transform.basis * Vector3(0, 1, 0)
+	camera_3d.look_at_from_position(to_global(vecyplane), canera_look_at.global_position, up)
+
+
+func _on_collide_area_area_entered(area):
+	if area is AreaGravity:
+		current_planet_area = area
+
+func _on_collide_area_area_exited(area):
+	if area is AreaGravity && area == current_planet_area:
+		current_planet_area = null
